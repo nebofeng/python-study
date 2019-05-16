2d0b5ca56b4b225cf55b64c75e31a6d348f99318
nebofeng <nebofeng@gmail.com>
2019-05-17 00:09:24 +0800

    --python re  demo

--- pers/nebo/python-hdfs/hdfsfilepath.py
+++ pers/nebo/python-hdfs/hdfsfilepath.py
@@ -1,5 +1,6 @@
 #! /bin/python
 #coding=utf-8
+import re
 '''
 根据文件名获取blocks 在linux上的location
 需要传递的参数：
@@ -7,15 +8,18 @@
 每个节点存放hdfs数据的目录： TODO: 改为配置文件读取，或是直接读取hdfs的配置文件。
 
 单个文件info:
-第 0 行: FSCK started by hdfs (auth:SIMPLE) from /28.28.33.73 for path /user/hbase/kill1 at Wed May 15 09:59:12 CST 2019
+
+FSCK started by hdfs (auth:SIMPLE) from /28.28.33.73 for path /user/hbase/kill1 at Wed May 15 09:59:12 CST 2019
 /user/hbase/kill1 118 bytes, replicated: replication=3, 1 block(s):  OK
 0. BP-540626610-10.58.33.81-1552889464019:blk_1073782666_42424 len=118 Live_repl=3  [DatanodeInfoWithStorage[28.28.33.72:50010,DS-34510fda-b7d7-4822-bde7-92827f4ac009,ARCHIVE], DatanodeInfoWithStorage[28.28.33.74:50010,DS-59b0f40d-bacc-4540-85d8-38d4e7ec5c59,DISK], DatanodeInfoWithStorage[28.28.33.73:50010,DS-a9dd6688-f6c1-478b-a558-771ec4598a37,ARCHIVE]]
-第 1 行: Status: HEALTHY
+
+Status: HEALTHY
  Number of data-nodes:	5
  Number of racks:		2
  Total dirs:			0
  Total symlinks:		0
-第 2 行: Replicated Blocks:
+
+Replicated Blocks:
  Total size:	118 B
  Total files:	1
  Total blocks (validated):	1 (avg. block size 118 B)
@@ -28,7 +32,8 @@
  Missing blocks:		0
  Corrupt blocks:		0
  Missing replicas:		0 (0.0 %)
-第 3 行: Erasure Coded Block Groups:
+
+Erasure Coded Block Groups:
  Total size:	0 B
  Total files:	0
  Total block groups (validated):	0
@@ -41,7 +46,8 @@
  Corrupt block groups:		0
  Missing internal blocks:	0
 FSCK ended at Wed May 15 09:59:12 CST 2019 in 0 milliseconds
-第 4 行: The filesystem under path '/user/hbase/kill1' is HEALTHY
+
+The filesystem under path '/user/hbase/kill1' is HEALTHY
 
 
 
@@ -62,12 +68,59 @@ FSCK ended at Wed May 15 09:59:12 CST 2019 in 0 milliseconds
 0. BP-540626610-10.58.33.81-1552889464019:blk_1073782681_42444 len=118 Live_repl=3  [DatanodeInfoWithStorage[28.
 '''
 
+
+
 import  os
+
+# def pareitem(item):
+
 hdfsfilename="/tmp/256Mfile0"
 hdfsfindcmd="sudo -u hdfs  hdfs fsck  {}  -files -blocks -locations".format(hdfsfilename)
 
-result=os.popen(hdfsfindcmd).read()
-resultarr=result.split("\n")
+#result=os.popen(hdfsfindcmd).read()
+
+result='''
+FSCK started by hdfs (auth:SIMPLE) from /28.28.33.73 for path /user/hbase/kill1 at Wed May 15 09:59:12 CST 2019
+/user/hbase/kill1 118 bytes, replicated: replication=3, 1 block(s):  OK
+0. BP-540626610-10.58.33.81-1552889464019:blk_1073782666_42424 len=118 Live_repl=3  [DatanodeInfoWithStorage[28.28.33.72:50010,DS-34510fda-b7d7-4822-bde7-92827f4ac009,ARCHIVE], DatanodeInfoWithStorage[28.28.33.74:50010,DS-59b0f40d-bacc-4540-85d8-38d4e7ec5c59,DISK], DatanodeInfoWithStorage[28.28.33.73:50010,DS-a9dd6688-f6c1-478b-a558-771ec4598a37,ARCHIVE]]
+
+Status: HEALTHY
+ Number of data-nodes:	5
+ Number of racks:		2
+ Total dirs:			0
+ Total symlinks:		0
+
+Replicated Blocks:
+ Total size:	118 B
+ Total files:	1
+ Total blocks (validated):	1 (avg. block size 118 B)
+ Minimally replicated blocks:	1 (100.0 %)
+ Over-replicated blocks:	0 (0.0 %)
+ Under-replicated blocks:	0 (0.0 %)
+ Mis-replicated blocks:		0 (0.0 %)
+ Default replication factor:	3
+ Average block replication:	3.0
+ Missing blocks:		0
+ Corrupt blocks:		0
+ Missing replicas:		0 (0.0 %)
+
+Erasure Coded Block Groups:
+ Total size:	0 B
+ Total files:	0
+ Total block groups (validated):	0
+ Minimally erasure-coded block groups:	0
+ Over-erasure-coded block groups:	0
+ Under-erasure-coded block groups:	0
+ Unsatisfactory placement block groups:	0
+ Average block group size:	0.0
+ Missing block groups:		0
+ Corrupt block groups:		0
+ Missing internal blocks:	0
+FSCK ended at Wed May 15 09:59:12 CST 2019 in 0 milliseconds
+
+The filesystem under path '/user/hbase/kill1' is HEALTHY
+'''
+resultarr=result.split("\n\n")
 
 '''
 从第二行开始，每一行都是一个blockid 以及三个备份节点
@@ -77,9 +130,15 @@ resultarr=result.split("\n")
 关系的参数： block个数 ，每个block 的备份个数，以及备份的节点，所在磁盘目录
 
 '''
-for i in range(len(resultarr)):
-    print ("第 {} 行: {} ".format(i,resultarr[i]))
+item=resultarr[0]
+items = item.split("\n")
 
+fileinfo = items[2]
+print(fileinfo)
+
+blknumpattern = re.compile(".*?=(\d), (\d) .*:  (.*)")#我们在编译这段正则表达式
+blknummatcher = re.match(blknumpattern,fileinfo)#在
+print(blknummatcher.group(3))
 
 #localget cmd 后面可以优化为多线程
 localdatadir="/home/hadoop"
