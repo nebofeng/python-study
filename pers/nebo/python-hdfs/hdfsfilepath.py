a4042241434ceb7a57b3196d506925f9093cf3ba
nebofeng <nebofeng@gmail.com>
2019-05-20 00:36:40 +0800

    --python re  get blkinfo

--- pers/nebo/python-hdfs/hdfsfilepath.py
+++ pers/nebo/python-hdfs/hdfsfilepath.py
@@ -74,13 +74,63 @@ import  os
 
 # def pareitem(item):
 
+
+
+
+
+
+
+def getfile(items):
+    fileinfo = items[1]
+    print(fileinfo)
+    filepattern = re.compile(".*?=(\d), (\d) .*:  (.*)")  # 我们在编译这段正则表达式
+    filematcher = re.match(filepattern, fileinfo)  # 在
+    filerep = filematcher.group(1)
+    fileblk = filematcher.group(2)
+    filebklsta = filematcher.group(3)
+    print("文件的备份是： ", filematcher.group(1))
+    print("文件的块是： ", filematcher.group(2))
+    for blkline in items[2:2 + int(fileblk)]:
+        getblock(blkline)
+
+
+
+
+def getblock(blkline):
+    print("blkline:",blkline)
+
+    blkpattern = re.compile("(\d).\s.*:(\w+)\s.*=(\d+)\s.*=(\d+)\s\s(.*)")  # 我们在编译这段正则表达式
+    blkmatcher = re.match(blkpattern, blkline)  # 在
+
+    print(blkmatcher.group())
+    blknum = blkmatcher.group(1)
+    blkid = blkmatcher.group(2)
+    blklen = blkmatcher.group(3)
+    blkrepnum = blkmatcher.group(4)
+    blkdatainfo=blkmatcher.group(5)
+    #[DatanodeInfoWithStorage[28.28.33.72:50010,DS-34510fda-b7d7-4822-bde7-92827f4ac009,ARCHIVE], DatanodeInfoWithStorage[28.28.33.74:50010,DS-59b0f40d-bacc-4540-85d8-38d4e7ec5c59,DISK], DatanodeInfoWithStorage[28.28.33.73:50010,DS-a9dd6688-f6c1-478b-a558-771ec4598a37,ARCHIVE]]
+    print(blknum,blkid,blklen,blkrepnum,blkdatainfo)
+
+    p1 = r"(?<=DatanodeInfoWithStorage)\[(\d+.\d+.\d+.\d+):\d+,\w+-\w+-\w+-\w+-\w+-\w+,\w+(?=])"#r"(?<=<h1>).+?(?=<h1>)
+    blkldp=re.compile(p1)#re.compile((?<=DatanodeInfoWithStorage).*(?=\\]>))
+    #blkldp=re.compile("DatanodeInfoWithStorage\\[(\d+.\d+.\d+.\d+)\:\d+,(.*)\\]") #DatanodeInfoWithStorag.e[(\w+):(\d+),.*
+    blkdatam=re.finditer(blkldp,blkdatainfo)
+
+    for match in blkdatam:
+        print("==============", match.group())
+        print("==============",match.group(1))
+        # print("==============", match.group(2))
+
+
+
+
+
 hdfsfilename="/tmp/256Mfile0"
 hdfsfindcmd="sudo -u hdfs  hdfs fsck  {}  -files -blocks -locations".format(hdfsfilename)
 
 #result=os.popen(hdfsfindcmd).read()
 
-result='''
-FSCK started by hdfs (auth:SIMPLE) from /28.28.33.73 for path /user/hbase/kill1 at Wed May 15 09:59:12 CST 2019
+result='''FSCK started by hdfs (auth:SIMPLE) from /28.28.33.73 for path /user/hbase/kill1 at Wed May 15 09:59:12 CST 2019
 /user/hbase/kill1 118 bytes, replicated: replication=3, 1 block(s):  OK
 0. BP-540626610-10.58.33.81-1552889464019:blk_1073782666_42424 len=118 Live_repl=3  [DatanodeInfoWithStorage[28.28.33.72:50010,DS-34510fda-b7d7-4822-bde7-92827f4ac009,ARCHIVE], DatanodeInfoWithStorage[28.28.33.74:50010,DS-59b0f40d-bacc-4540-85d8-38d4e7ec5c59,DISK], DatanodeInfoWithStorage[28.28.33.73:50010,DS-a9dd6688-f6c1-478b-a558-771ec4598a37,ARCHIVE]]
 
@@ -132,13 +182,13 @@ resultarr=result.split("\n\n")
 '''
 item=resultarr[0]
 items = item.split("\n")
+getfile(items)
 
-fileinfo = items[2]
-print(fileinfo)
 
-blknumpattern = re.compile(".*?=(\d), (\d) .*:  (.*)")#我们在编译这段正则表达式
-blknummatcher = re.match(blknumpattern,fileinfo)#在
-print(blknummatcher.group(3))
+
+
+
+
 
 #localget cmd 后面可以优化为多线程
 localdatadir="/home/hadoop"
